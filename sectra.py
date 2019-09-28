6e223828d993fbcd12d823000391443af738bdd2
Alexander Dahl <aleda145@student.liu.se>
2019-09-28 19:20:37 +0200

    split string and have print funcitonality

--- sectra.py
+++ sectra.py
@@ -1,6 +1,12 @@
 while True:    
     input_data=input("enter input")
+    
     print(input_data)
     if input_data=="quit":
         print("sequence finished, exiting")
+        #after this all prints should happen
         break;
+    split_input=input_data.split(" ")
+    print(split_input)
+    if split_input[0]=="print":
+        print("now do prints")
