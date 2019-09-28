a8cfa0723bf77d11ab7f1e535404a3172e93d7c4
Alexander Dahl <aleda145@student.liu.se>
2019-09-28 20:08:57 +0200

    working example for add with registers

--- sectra.py
+++ sectra.py
@@ -1,6 +1,12 @@
 allowed_operands=['ADD','SUBTRACT','MULTIPLY']
 registers={}
 
+def is_integer(val):
+    try:
+        integer=int(val)
+        return True
+    except ValueError:
+        return False
 while True:    
     input_data=input("enter input").upper()
     # A requirement is for it to be case insensitive. Solved by only dealing with CAPITALS
@@ -19,12 +25,12 @@ while True:
             else:
                 print("You input only one command, but is not a quit. Error!")
 
-        if len(split_input)==2:
+        elif len(split_input)==2:
             if split_input[0]=="PRINT":
                 print("now do prints")
             else:
                 print("You input two commands, but the first one was not a print. Error!")
-        if len(split_input)==3:
+        elif len(split_input)==3:
             if split_input[1] not in allowed_operands:
                 print("this operand is currently not supported")
             else:
@@ -32,9 +38,27 @@ while True:
 
                 # if gotten to this point the register will be a key in the registers dict
                 key = split_input[0]
+
+                # check first if key exists in registers. If not give it the value 0.
+                # What will happen if reg1 MULTIPLY 5 happens when reg1 has no unassigned val?
+                if key not in registers:
+                    registers[key]=0
+                print(registers)    
                 operand = split_input[1]
                 val_or_key = split_input[2]
-                    
+                #since both values and registers can be in the third argument
+                # it is necessary to check if it exists in the register dict
+                # before doing any operations
+                if val_or_key in registers:
+                    key2=val_or_key
+                elif is_integer(val_or_key):
+                    print('its an int!')
+                    if operand == 'ADD':
+                      registers[key]=registers[key]+int(val_or_key)
+                    print(registers)
+                else:
+                    print("this is not an integer, please enter integers!")
+                    #if not it has to be a number, which has to be checked.
         else:
             print("You entered too many commands! Error!")
     else:
