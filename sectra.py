44462fdc2488021ecf8cd407064bdcf06386dcbd
Alexander Dahl <aleda145@student.liu.se>
2019-09-28 19:36:49 +0200

    basic control flow done

--- sectra.py
+++ sectra.py
@@ -1,12 +1,25 @@
+allowed_operands=['ADD','SUBTRACT','MULTIPLY']
+
 while True:    
-    input_data=input("enter input")
-    
+    input_data=input("enter input").upper()
+    # A requirement is for it to be case insensitive. Solved by only dealing with CAPITALS
+
     print(input_data)
-    if input_data=="quit":
-        print("sequence finished, exiting")
-        #after this all prints should happen
-        break;
     split_input=input_data.split(" ")
     print(split_input)
-    if split_input[0]=="print":
-        print("now do prints")
+    if len(split_input)==1:
+        if split_input[0]=="QUIT":
+            print("sequence finished, exiting")
+            #after this all prints should happen
+            break;
+
+    if len(split_input)==2:
+        if split_input[0]=="PRINT":
+            print("now do prints")
+        else:
+            print("You input two commands, but the first one was not a print. Error!")
+    if len(split_input)==3:
+
+        if split_input[1] not in allowed_operands:
+            print("this operand is currently not supported")
+    
